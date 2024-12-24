51e994661f82a88760e37b5b2b1e29d41cb58f10
Charité Igirimbabazi <62570559+loochaa@users.noreply.github.com>
2024-12-24 14:19:28 -0500

    added the get_bet function

--- Slot-Machine/src/main.py
+++ Slot-Machine/src/main.py
@@ -1,5 +1,7 @@
 # gloabal variables
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True: # continue prompting until a valid amout is entered
@@ -28,8 +30,22 @@ def get_number_of_lines():
             print("Please input a number.")
     return lines
 
+def get_bet():
+    while True: # continue prompting until a valid amout is entered
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit(): # checking if the input is a number
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break #break out of the loop
+            else:
+                print("Amount must be greater than between $" + str(MIN_BET) + " and $" + str(MAX_BET) + ".")
+        else:
+            print("Please input a number.")
+    return amount
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    print(f"You are betting ${balance} on {lines} lines. Ready to play?")
     
     
     
