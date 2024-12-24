99e0e28b9eaaedf9a4f1db1b09f71c684debba4a
Charité Igirimbabazi <62570559+loochaa@users.noreply.github.com>
2024-12-24 14:16:30 -0500

    added get_number and main  functions

--- Slot-Machine/src/main.py
+++ Slot-Machine/src/main.py
@@ -1,3 +1,6 @@
+# gloabal variables
+MAX_LINES = 3
+
 def deposit():
     while True: # continue prompting until a valid amout is entered
         amount = input("What would you like to deposit? $")
@@ -10,3 +13,24 @@ def deposit():
         else:
             print("Please input a number.")
     return amount
+
+
+def get_number_of_lines():
+    while True: # continue prompting until a valid amout is entered
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit(): # checking if the input is a number
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break #break out of the loop
+            else:
+                print(f"Enter a valid number of lines (1-{MAX_LINES})")
+        else:
+            print("Please input a number.")
+    return lines
+
+def main():
+    balance = deposit()
+    
+    
+    
+main()
